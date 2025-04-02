e8f34f7169ff57076e479afa6e69bdccfe6af42a
Michael Herger <michael@herger.net>
2025-04-02 16:28:24 +0200

    Use `Data::Dumper` rather than `Data::Dump`, as the latter is not installed by default.

--- buildme.pl
+++ buildme.pl
@@ -523,8 +523,8 @@ sub buildDockerImage {
 			" --tag ghcr.io/$org/$defaultDestName:$_";
 		} @tags);
 
-use Data::Dump;
-warn Data::Dump::dump(\@tags, $tags);
+use Data::Dumper;
+warn Dumper(\@tags, $tags);
 
 		system("cd $workDir; docker buildx build --push --platform linux/arm/v7,linux/amd64 $tags .");
 	}
